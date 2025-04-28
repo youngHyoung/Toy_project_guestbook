aa17d4d33ea9700fe67b54cb2a19b61b7aa28211
hoyoung <hoyoungjin9148@gmail.com>
2025-04-28 23:29:09 +0900

    main.js에 GET 추가하기
    기존에 POST와 DELETE로만 이루어지던 로직에 GET을 추가하여 웹사이트에 사용자의 방문록이 기록될 수 있도록 수정하였고, 이 과정에서 DELETE문을 함께 GET 로직 안으로 옮겼음.
    
    전체 로직은 사용자가 서버에 POST하면, POST 후 그 내용에서의 ID 값을 받고, 그 ID값을 파라미터로 받는 GET 함수를 실행하여 웹사이트에 사용자가 POST한 내용이 드러나도록 설계하였음.

--- main.js
+++ main.js
@@ -1,3 +1,82 @@
+async function loadGuestbook(thatId) { //thatId 값 가져오기
+  try{
+    const response = await fetch("http://52.79.131.217:8000/guestbook/",{ //서버에 GET날리기
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`서버 응답 오류: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    const data = result.data
+    const targetData = data.find(data => data.id === thatId); //데이터 배열 중 해당 아이디 값을 가지고 있는 값 찾기
+    if (!targetData) {
+      throw new Error(`ID ${thatId} 값 부재`);
+    }
+    
+    console.log(targetData); // data 배열 중 목표 데이터 수집
+
+    const responseContainer = document.getElementById('response-container');
+
+    
+      const written = document.createElement('div');
+      written.className = 'written';
+      
+      written.innerHTML = `
+      <p><strong>😁제목:</strong> ${targetData.title || '미지정'}</p>
+      <p><strong>🔖이름:</strong> ${targetData.name || '미지정'}</p>
+      <p><strong>✍️내용입력:</strong> ${targetData.content || '미지정'}</p>
+      <p><strong>🌱방문일:</strong> ${targetData.created.slice(0, 10) || '미지정'}</p>
+      <p><strong>🤐비밀번호:</strong> <input type="text" class="password" name="password" placeholder="****" required></p>
+      <button class="del-button">삭제하기</button> 
+      `;
+
+    const deleteButton = written.querySelector('.del-button');
+    deleteButton.addEventListener('click', async () =>{
+    const passwordInput = written.querySelector('.password');
+    const password = passwordInput.value;
+
+    try {
+      const deleteResponse = await fetch(`http://52.79.131.217:8000/guestbook/`,{
+        method: "DELETE",
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+          id: String(targetData.id),
+          password,
+        }),
+      });
+
+      if (!deleteResponse.ok) { // 삭제 실패시 throw Error해서 written,remove 구문 건너뜀뜀
+        const errorMessage = await deleteResponse.text();
+        alert('비밀번호가 틀렸습니다!')
+        throw new Error(errorMessage.error)
+      }
+
+      written.remove();
+      alert('방명록 삭제 완료')
+
+    } catch (e) {
+      console.log('오류 발생')
+      console.log(e.message);
+    }
+  })
+
+  responseContainer.appendChild(written);
+
+
+} catch (e) {
+  console.error('방명록 로드 오류:', e.message)
+}
+}
+
+
+//POST하기
 document.getElementById("form").addEventListener('submit', async function(event) {
   event.preventDefault();
   const form = event.target //form 요소 타겟
@@ -21,68 +100,20 @@ document.getElementById("form").addEventListener('submit', async function(event)
     throw new Error(`서버 응답 오류: ${response.status}`);
   }
   
-  
+  result = await response.json(); 
+  console.log(result); // 데이터 받은 값 확인하기
 
-  const result = await response.json(); // json 서버 응답 객체로 변환
-  console.log(result) // 서버 응답
-
-  const data = result.data;
-
-  const responseContainer = document.getElementById("response-container");
-
-  const written = document.createElement('div');
-  written.className = 'written';
-  written.innerHTML = `
-    <p><strong>😁제목:</strong> ${data.title || '미지정'}</p>
-    <p><strong>🔖이름:</strong> ${data.name || '미지정'}</p>
-    <p><strong>✍️내용입력:</strong> ${data.content || '미지정'}</p>
-    <p><strong>🌱방문일:</strong> ${data.created.slice(0, 10) || '미지정'}</p>
-    <p><strong>🤐비밀번호:</strong> <input type="text" class="password" name="password" placeholder="****" required></p>
-    <button class="del-button">삭제하기</button>    
-    `;
-
-  responseContainer.appendChild(written); // <div class="written"></div>를 자식요소로 추가
-    //삭제하기 버튼 이벤트 입력
-  const deleteButton = written.querySelector('.del-button');
-  deleteButton.addEventListener('click', async () =>{
-    const passwordInput = written.querySelector('.password');
-    const password = passwordInput.value;
-
-    try {
-      const deleteResponse = await fetch(`http://52.79.131.217:8000/guestbook/`,{
-        method: "DELETE",
-        headers: {
-          'Content-Type' : 'application/json',
-        },
-        body: JSON.stringify({
-          id: String(data.id),
-          password,
-        }),
-      });
-
-      if (!deleteResponse.ok) { // 삭제 실패시 throw Error해서 written,remove 구문 건너뜀뜀
-        const errorMessage = await deleteResponse.text();
-        alert('비밀번호가 틀렸습니다!')
-        throw new Error(errorMessage.error)
-      }
-
-      written.remove();
-      alert('방명록 삭제 완료')
-
-    } catch (e) {
-      console.log('오류 발생')
-      console.log(e.message);
-    }
-
-  })
-
-
+  const thatId = result.data.id; // 해당 POST에서 아이디 값 추출
+  await loadGuestbook(thatId); //방명록 추가 후 갱신(id값 넣어주기)
   
 } catch(e) {
   console.log('오류 발생')
   console.log(e.message);
   }
 })
+
+
+
 
 
 
